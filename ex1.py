e3b7ddb2f64d8fa41801110366bcf1b2070d5bd5
ctgccc <144323316+ctgccc@users.noreply.github.com>
2023-09-20 12:37:57 +0300

    ex1.py

--- ex1.py
+++ ex1.py
@@ -1,10 +1,16 @@
-a = input("Введите целое число: ")
+number = int(input("Введите натуральное число: "))
+prev_digit = 10
+is_ordered = True
 
-for i in range(len(a)):
-    if a[i] < a[i - 1]:
-        i += 1
-        b = "Числа упорядочены по убыванию"
-    else:
-        b = "Числа упорядочены не по убыванию"
+while number > 0:
+    digit = number % 10
+    if digit > prev_digit:
+        is_ordered = False
+        break
+    prev_digit = digit
+    number //= 10
 
-print(b)
+if is_ordered:
+    print("Последовательность цифр упорядочена по убыванию")
+else:
+    print("Последовательность цифр не упорядочена по убыванию")
